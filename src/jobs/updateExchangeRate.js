643912931c0d43de4c8d7552c11664934da77035
st3pan <khylkostepan@gmail.com>
2022-11-07 09:52:37 +0200

    dependencies & refactoring

--- src/jobs/updateExchangeRate.js
+++ src/jobs/updateExchangeRate.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
-const USD = 'USD';
-const EUR = 'EUR';
+const availableCurrencies = ['USD', 'EUR'];
 
 const apiURL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
 
@@ -10,27 +9,18 @@ module.exports = async (app) => {
   const { exchangeRate } = db;
 
   const { data } = await axios.get(apiURL);
-  const apiUSD = data.find((currency) => currency.ccy === USD);
-  const apiEUR = data.find((currency) => currency.ccy === EUR);
 
-  const usdTotal = await exchangeRate.count({ currency: USD });
-  const eurTotal = await exchangeRate.count({ currency: EUR });
+  await Promise.all(availableCurrencies.map(async (currency) => {
+    const apiCurrency = data.find((c) => c.ccy === currency);
 
-  if (typeof apiUSD === 'object' && apiUSD.sale) {
-    const usdRate = parseFloat(apiUSD.sale).toFixed(2);
-    if (usdTotal === '0') {
-      await exchangeRate.save({ currency: USD, rate: usdRate });
-    } else {
-      await exchangeRate.update({ currency: USD }, { rate: usdRate });
+    if (typeof apiCurrency === 'object' && apiCurrency.sale) {
+      const count = await exchangeRate.count({ currency });
+      const rate = parseFloat(apiCurrency.sale).toFixed(2);
+      if (count === '0') {
+        await exchangeRate.save({ currency, rate });
+      } else {
+        await exchangeRate.update({ currency }, { rate });
+      }
     }
-  }
-
-  if (typeof apiEUR === 'object' && apiEUR.sale) {
-    const eurRate = parseFloat(apiEUR.sale).toFixed(2);
-    if (eurTotal === '0') {
-      await exchangeRate.save({ currency: EUR, rate: eurRate });
-    } else {
-      await exchangeRate.update({ currency: EUR }, { rate: eurRate });
-    }
-  }
+  }));
 };
